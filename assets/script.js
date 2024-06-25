caf7517f9c613c58d1d70fe1c53b30ba13e46110
dutes <dutes@users.noreply.github.com>
2024-06-25 17:53:10 +0000

    FIX: Gallery page content and added CSS styling. - Centered the gallery images in a grid - added padding and margins for images - applied box shadows to images and gallery. - added hover effects on images. - added pointer cusors on all user selectable images.

--- assets/script.js
+++ assets/script.js
@@ -1,135 +1,133 @@
-
 //event listener for page changes to move through states
-document.addEventListener('DOMContentLoaded', () => {
-    const firstPage = document.getElementById('first-page');
-    const galleryPage = document.getElementById('gallery-page');
-    const gamePage = document.getElementById('game-page');
-    const successPage = document.getElementById('success-page');
-    const chosenImage = document.getElementById('chosen-image');
-    const priceElement = document.getElementById('price');
-    const totalElement = document.getElementById('total');
-    const successSound = document.getElementById('success-sound');
+document.addEventListener("DOMContentLoaded", () => {
+  const firstPage = document.getElementById("first-page");
+  const galleryPage = document.getElementById("gallery-page");
+  const gamePage = document.getElementById("game-page");
+  const successPage = document.getElementById("success-page");
+  const chosenImage = document.getElementById("chosen-image");
+  const priceElement = document.getElementById("price");
+  const totalElement = document.getElementById("total");
+  const successSound = document.getElementById("success-sound");
 
-    let price=0;
-    let total=0;
+  let price = 0;
+  let total = 0;
 
- // Initialize to show the first page
-    showSection('first-page');
+  // Initialize to show the first page
+  showSection("first-page");
 
-//search functionality
-document.getElementById('search-bar').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const query = document.getElementById('search-query').value; //get the user input
-    if (query) {
-        searchQueryImage(query + ' toy'); //append toy to the user search query
-    }
-});
+  //search functionality
+  document
+    .getElementById("search-bar")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      const query = document.getElementById("search-query").value; //get the user input
+      if (query) {
+        searchQueryImage(query + " toy"); //append toy to the user search query
+      }
+    });
 
-//function to take user input, form a url, pass it to google and get the images back
-function searchQueryImage(query) {
-    const apiKey = 'AIzaSyDfW4eFArqNyrreygkdM8GPcDzgneDC_h0';
-    const cx = '40e807cc4d6bf4848';
-    const url = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cx}&q=${encodeURIComponent(query)}&searchType=image&num=5`; 
+  //function to take user input, form a url, pass it to google and get the images back
+  function searchQueryImage(query) {
+    const apiKey = "AIzaSyDfW4eFArqNyrreygkdM8GPcDzgneDC_h0";
+    const cx = "40e807cc4d6bf4848";
+    const url = `https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=${cx}&q=${encodeURIComponent(
+      query
+    )}&searchType=image&num=5`;
     //https://developers.google.com/custom-search/v1/introduction -> google custom search docs
 
-    console.log('Fetching URL:', url); //debug output
+    console.log("Fetching URL:", url); //debug output
 
     fetch(url) //code to use custom google search and pass the data off to the displayimages function or if none, displaynoresults
-        .then(response => response.json())
-        .then(data => {
-            console.log('Data received:', data); //debug output
-            if (data.items && data.items.length > 0) {
-                displayImages(data.items);
-                showSection('gallery-page');
-            } else {
-                displayNoResults();
-            }
-        })
-        .catch(error => console.error('Fetch error:', error)); //debug
-}
-
-//function to display images in the search result container
-function displayImages(items) {//runs through the returned json blob and pulls out the image link, title and sets size to 200px
-    const resultsContainer = document.getElementById('gallery-page');
-    resultsContainer.innerHTML = ''; 
-
-    items.forEach(item => {
-        console.log('Item:', item); // Debugging: log each item
-        const imgElement = document.createElement('img');
-        imgElement.src = item.link;
-        imgElement.alt = item.title;
-        imgElement.style.width='200px';
-        imgElement.addEventListener('click', () => {
-            console.log('image clicked', item.link);
-            chosenImage.src=item.link;
-            price= generateRandomPrice();
-            priceElement.textContent=`${price.toFixed(2)}€`;
-            total=0;
-            totalElement.textContent=`${total.toFixed(2)}€`;
-            showSection('game-page');
-        })
-        resultsContainer.appendChild(imgElement);       
-    });
-}
-
-//function to output message when no images were found
-function displayNoResults() {
-    const resultsContainer = document.getElementById('seaerch-results');
-    resultsContainer.innerHTML = '<p>No images found</p>';
-}
-
-
-//function to show the active section
-function showSection(sectionId) {
-    console.log('trying to load', sectionId);
-    firstPage.style.display = 'none';
-    galleryPage.style.display ='none';
-    gamePage.style.display = 'none';
-    successPage.style.display = 'none';
-
-    const sectionToShow=document.getElementById(sectionId);
-    if (sectionToShow) {
-        sectionToShow.style.display='flex';
-        console.log('Section shown:', sectionId);
-    } else {
-        console.error('section not found', sectionId);
-    }
-}
-
-function generateRandomPrice() {
-    const coinValues = [0.05, 0.10,0.20, 0.50, 1.00, 2.00];
-
-    let maxPrice= 5.00;
-    let minPrice= 0.05; 
-
-    let steps = (maxPrice - minPrice) / 0.05; //establish range and number of steps between 0.05 and 5
-
-    let randomStep = Math.floor(Math.random() * steps); //make a random increment of steps
-
-    let price = (minPrice + randomStep * 0.05).toFixed(2); //make the step number into a money amount. 
-
-    return parseFloat(price);
-}
-
-document.querySelectorAll('.coin').forEach(coin => {
-    coin.addEventListener('click', () => {
-        const coinValue = parseFloat(coin.getAttribute('data-value'));
-        total += coinValue;
-        totalElement.textContent = `${total.toFixed(2)}€`;
-        if (total.toFixed(2) === price.toFixed(2)) {
-            successSound.play();
-            showSection('success-page');
-            toyDescription.textContent ='Would you like to another of the same toys or start with a new search?';
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data received:", data); //debug output
+        if (data.items && data.items.length > 0) {
+          displayImages(data.items);
+          showSection("gallery-page");
+        } else {
+          displayNoResults();
         }
+      })
+      .catch((error) => console.error("Fetch error:", error)); //debug
+  }
+
+  //function to display images in the search result container
+  function displayImages(items) {
+    //runs through the returned json blob and pulls out the image link, title and sets size to 200px
+    const resultsContainer = document.getElementById("search-result");
+    resultsContainer.innerHTML = "";
+
+    items.forEach((item) => {
+      console.log("Item:", item); // Debugging: log each item
+      const imgElement = document.createElement("img");
+      imgElement.src = item.link;
+      imgElement.alt = item.title;
+      imgElement.style.width = "200px";
+      imgElement.addEventListener("click", () => {
+        console.log("image clicked", item.link);
+        chosenImage.src = item.link;
+        price = generateRandomPrice();
+        priceElement.textContent = `${price.toFixed(2)}€`;
+        total = 0;
+        totalElement.textContent = `${total.toFixed(2)}€`;
+        showSection("game-page");
+      });
+      resultsContainer.appendChild(imgElement);
     });
-});  
+  }
 
-document.getElementById('continue-game').addEventListener('click', () => {
-    showSection('gallery-page');
-});
+  //function to output message when no images were found
+  function displayNoResults() {
+    const resultsContainer = document.getElementById("gallery-page");
+    resultsContainer.innerHTML = "<p>No images found</p>";
+  }
 
-document.getElementById('new-game').addEventListener('click', () => {
-    showSection('first-page');
-})
+  //function to show the active section
+  function showSection(sectionId) {
+    console.log("trying to load", sectionId);
+    firstPage.style.display = "none";
+    galleryPage.style.display = "none";
+    gamePage.style.display = "none";
+    successPage.style.display = "none";
 
+    const sectionToShow = document.getElementById(sectionId);
+    if (sectionToShow) {
+      sectionToShow.style.display = "flex";
+      console.log("Section shown:", sectionId);
+    } else {
+      console.error("section not found", sectionId);
+    }
+  }
+
+  function generateRandomPrice() {
+    const coinValues = [0.05, 0.1, 0.2, 0.5, 1.0, 2.0];
+    let maxPrice = 5.0;
+    let minPrice = 0.05;
+    let steps = (maxPrice - minPrice) / 0.05; //establish range and number of steps between 0.05 and 5
+    let randomStep = Math.floor(Math.random() * steps); //make a random increment of steps
+    let price = (minPrice + randomStep * 0.05).toFixed(2); //make the step number into a money amount.
+    return parseFloat(price);
+  }
+
+  document.querySelectorAll(".coin").forEach((coin) => {
+    coin.addEventListener("click", () => {
+      const coinValue = parseFloat(coin.getAttribute("data-value"));
+      total += coinValue;
+      totalElement.textContent = `${total.toFixed(2)}€`;
+      if (total.toFixed(2) === price.toFixed(2)) {
+        successSound.play();
+        showSection("success-page");
+        toyDescription.textContent =
+          "Would you like to another of the same toys or start with a new search?";
+      }
+    });
+  });
+
+  document.getElementById("continue-game").addEventListener("click", () => {
+    showSection("gallery-page");
+  });
+
+  document.getElementById("new-game").addEventListener("click", () => {
+    showSection("first-page");
+  });
 });
